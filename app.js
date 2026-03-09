709d3243035acc18fe3be43258e948b7bd88c783
keerthivatshiniramesh <keerthivarshini.ramesh@gmail.com>
2026-03-09 12:10:56 +0530

    fix email

--- app.js
+++ app.js
@@ -24,7 +24,6 @@ app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 })
 
-
 Mongoose.connect(process.env.MongoDBURI)
     .then(() => {
         console.log('MongoDB connected succesfully!');
